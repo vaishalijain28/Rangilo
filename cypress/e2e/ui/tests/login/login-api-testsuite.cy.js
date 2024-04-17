2ea79bc512f49002d8f4d2801d0864bb7520473b
vaishalijain28 <vaishalijain.28.vj@gmail.com>
2024-04-17 14:48:16 +0530

    commit -m "17-04-2024"

---
+++ cypress/e2e/ui/tests/login/login-api-testsuite.cy.js
@@ -0,0 +1,78 @@
+// import AccountApiHelper
+
+import { TestTag } from "../../constants/test-tags-constants";
+
+const accountApiHelper = new AccountApiHelper()
+
+describe (
+    'Login Suite',
+    {
+        tags: TestTag.Login,
+    },
+    () => {
+        const username = Cypress.env('credentials').loginCustomerEmail
+        const password = Cypress.env('credentials').genericCustomerPassword
+        const invalidUsername = Cypress.env('credentials').invalidStagingUser
+        const invalidPassword = Cypress.env('credentials').invalidStagingPassword
+
+
+        it(
+            'It should be able to login using valid credentials <positive> -(POST) -/Account/Login',
+            {
+                tags: [
+                    TestTag.Login,
+                    TestTag.Smoke,
+                    TestTag.API,
+                    TestTag.Regression,
+                    TestTag.AllTypes,
+                    TestTag.AllModules,
+                ],
+            },
+            () => {
+                accountApiHelper.login(
+                    {
+                        userName: username,
+                        password: password,
+                    },
+                    false
+                )
+
+                cy.get('@postResponse').then((postResonse) => {
+                    expect(postResponse.status).to.eq(200)
+                    expect(postResponse.isOKStatusCode).to.eq(true)
+                    expect(postResponse.statusText).to.eq('OK')
+                })
+            }
+        )
+
+        it(
+            'It should not be able to login using invalid credentials <negative> - (POST) -/Account/Login',
+            {
+                tags:[
+                    TestTag.Login,
+                    TestTag.Smoke,
+                    TestTag.API,
+                    TestTag.Regression,
+                    TestTag.AllTypes,
+                    TestTag.AllModules,
+                ],
+            },
+            () => {
+                accountApiHelper.login(
+                    {
+                        userName: invalidUsername,
+                        password: invalidPassword,
+                    },
+                    false
+                )
+                cy.get('@postResponse').then((postResponse) => {
+                    console.log(postResponse.body)
+                    expect(postResponse.status).to.eq(401)
+                    expect(postResponse.isOKStatusCode).to.eq(false)
+                    expect(postResponse.statusText).to.eq('Unauthorized')
+
+                })
+            }
+        )
+    }
+)
